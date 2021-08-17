175e13193109943f3b406ae35c98a24ea2c6ed51
Sadab Ali <84293105+sadabjr@users.noreply.github.com>
2021-08-18 01:13:54 +0530

    Add files via upload

---
+++ pattern_printing.py
@@ -0,0 +1,12 @@
+# num = int(input("Inter trhe numner of row:"))
+# for i in range(1, num+1):
+#     for j in range(1, i+1):
+#         print("*", end=" ")
+#     print()
+
+# num1 = int(input("Inter the number of the row : "))
+# for i in range(num1):
+#     for j in range(i+1):
+#         print(j+1, end=" ")
+#     print()
+
